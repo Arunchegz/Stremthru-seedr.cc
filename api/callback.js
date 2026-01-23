eab773f14fbc0b75645fde2d6dbf15d33b2219c6
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-01-23 10:39:52 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -9,7 +9,13 @@ export default async function handler(req, res) {
     }
 
     const { data } = await axios.get(
-      `https://www.seedr.cc/rest/device/code?device_code=${deviceCode}`
+      `https://www.seedr.cc/rest/device/code?device_code=${deviceCode}`,
+      {
+        headers: {
+          "User-Agent": "Seedr-Stremio-Addon",
+          "Accept": "application/json"
+        }
+      }
     );
 
     if (!data.access_token) {
@@ -21,13 +27,10 @@ export default async function handler(req, res) {
       <h2>Authorization successful!</h2>
       <p>Your access token:</p>
       <pre>${data.access_token}</pre>
-      <p>Now save it in Vercel:</p>
-      <pre>vercel env add SEEDR_TOKEN</pre>
-      <p>Paste this token when asked, then redeploy:</p>
-      <pre>vercel --prod</pre>
+      <p>Save this in Vercel as <b>SEEDR_TOKEN</b></p>
     `);
   } catch (err) {
-    console.error(err.message);
+    console.error(err.response?.data || err.message);
     res.status(500).send("Authorization failed");
   }
 }
